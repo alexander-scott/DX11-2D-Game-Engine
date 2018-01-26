0bbbc12512ee752e2402255102055678c4008add
Alex Scott <alexanderscott@outlook.com>
2018-01-26 13:36:19 +0000

    Refactored ObjectManager class

--- Engine/ObjectManager.h
+++ Engine/ObjectManager.h
@@ -24,6 +24,28 @@ public:
 
 private:
 	IComponent* CreateComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+
+	TransformComponent* ParseTransformComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	SpriteRendererComponent* ParseSpriteRendererComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	SpriteAnimatorComponent* ParseSpriteAnimatorComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	RigidBodyComponent* ParseRigidBodyComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	TextRendererComponent* ParseTextRendererComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	CircleColliderComponent* ParseCircleColliderComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	BoxColliderComponent* ParseBoxColliderComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	TiledBGRenderer* ParseTiledBGRenderer(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	PlayerComponent* ParsePlayerComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	DamageableComponent* ParseDamageableComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	ProjectileComponent* ParseProjectileComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	AIAgentComponent* ParseAIAgentComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	ProjectileManagerComponent* ParseProjectileManagerComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	TriggerBoxComponent* ParseTriggerBoxComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	GameCameraComponent* ParseGameCameraComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	GUITextComponent* ParseGUITextComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	GUITextValueComponent* ParseGUITextValueComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	GUITextDamageComponent* ParseGUITextDamageComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	GUIButtonComponent* ParseGUIButtonComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	GUISpriteRendererComponent* ParseGUISpriteRendererComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
+	GameManagerComponent* ParseGameManagerComponent(shared_ptr<GameObject> go, xml_node<>* node, ICameraGameObject* cam);
 	
 	int GenerateNewID();
 
